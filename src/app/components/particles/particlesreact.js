a29c860719b5c8876973ea42651283b11945d9e1
kyy-95631488 <riskir5622@gmail.com>
2025-05-04 14:28:36 +0700

    Update Commit [ 04 - 05 - 2025 | 14:28 ]

--- src/app/components/particles/particlesreact.js
+++ src/app/components/particles/particlesreact.js
@@ -4,7 +4,7 @@ import { useEffect, useMemo, useState } from "react";
 import { loadSlim } from "@tsparticles/slim";
 
 const ParticlesComponent = (props) => {
-    const [init, setInit] = useState(false);
+    const [setInit] = useState(false);
     useEffect(() => {
         initParticlesEngine(async (engine) => {
             await loadSlim(engine);
